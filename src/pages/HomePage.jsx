6166f9a9a481df188d2030581103b20c4c0898ef
eradvarma <eradvarma@gmail.com>
2025-08-12 10:09:06 +0530

    Home - CRDT at a glance Done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection'
 import AboutCrdt from '../components/home/AboutCrdt'
 import VisionMission from '../components/home/VisionMission'
 import DirectorMessage from '../components/home/DirectorMessage'
+import ProfessorsSlider from '../components/home/ProfessorsSlider'
 
 const HomePage = () => {
     return (
@@ -11,6 +12,7 @@ const HomePage = () => {
             <AboutCrdt />
             <VisionMission />
             <DirectorMessage />
+            {/* <ProfessorsSlider /> */}
         </div>
     )
 }
